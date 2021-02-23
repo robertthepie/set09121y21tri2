cea11878d9e2365247889276484f120f2003ef0f
robertthepie <stanlytheplatypus@gmail.com>
2021-02-23 13:32:20 +0000

    Create Source.cpp
    Source.cpp contains the code that will be the foundation for nearly any code if not all that i will write

---
+++ 01_practical/Source.cpp
@@ -0,0 +1,50 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+const int gameWidth = 800;
+const int gameHeight = 600;
+
+void Reset() {
+
+}
+
+void Load() {
+
+}
+
+void Update(RenderWindow& window) {
+    // Reset clock, recalculate deltatime
+    static Clock clock;
+    float dt = clock.restart().asSeconds();
+    // check and consume events
+    Event event;
+    while (window.pollEvent(event)) {
+        if (event.type == Event::Closed) {
+            window.close();
+            return;
+        }
+    }
+
+    // Quit Via ESC Key
+    if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+        window.close();
+    }
+}
+
+void Render(RenderWindow& window) {
+
+}
+
+int main() {
+    RenderWindow window(VideoMode(gameWidth, gameHeight), "PONG");
+    Load();
+    while (window.isOpen()) {
+        window.clear();
+        Update(window);
+        Render(window);
+        window.display();
+    }
+    return 0;
+}
